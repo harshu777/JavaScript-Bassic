9304b043d3c4d347d301429becb11d2f107f617b
harshu777 <hbaviskar777@gmail.com>
2020-09-23 23:05:35 +0530

    array basic

---
+++ index.js
@@ -0,0 +1,43 @@
+// console.log("hello world");
+let name = 'harsh';
+console.log(name);
+let firstName = 'harsh';
+const lastName = 'baviskar';   //const is set this variable to unchangable
+// lastName = 'bavisk'
+console.log(firstName,lastName)
+console.log(lastName)
+
+// primitive types / value types  [string,number,boolean,unidentified,null]
+let age = 27;  //  number
+let isAproved = true;  //boolean
+let selectedColor = null;  
+let fName = undefined;
+
+// dynamic typing
+//to check type 
+typeof age
+console.log(age);
+// in JavaScript we dont have type as float
+
+
+//object types  [object,array,function]
+ //object creation
+let person = {
+    name: 'harsh',
+    age: 27
+};
+person.age = 30
+
+console.log(person.age);
+
+//arrays
+let selectedColors = ['red','blue'];
+let friendsName = ['ross','joey','chandlar','rechal','phoebe'];
+// selectedColors[2] = 'grey';
+// selectedColors[3] = 20;
+// console.log(selectedColors);
+// console.log(selectedColors[0]);
+// console.log(selectedColors.)
+// typeof selectedColors
+console.log(friendsName);
+console.log(friendsName[0]);
